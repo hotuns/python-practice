602835bc5ef55374f299d3cea587249f1f201c40
何东树 <hedongshu@egret.com>
2019-09-16 12:11:35 +0800

    改进updata功能

--- README.md
+++ README.md
@@ -5,7 +5,18 @@
 
 
 ---
+* [getDate](https://github.com/hedongshu/python-practice/getDate) 
+    
+把文件夹内的所有文件名(还有文件内的字段),从时间戳格式修改成人类可读的格式
 
-* [getDate](https://github.com/hedongshu/python-practice)
-	把时间戳格式的文件名改成人类习惯的格式。
 
+* [updateREADME](https://github.com/hedongshu/python-practice/updateREADME) 
+    执行之后会自动更新该项目的README
+名字: 根据文件夹名
+描述: 根据index文件头部的描述信息
+
+* [characterImage](https://github.com/hedongshu/python-practice/characterImage) 
+    把图片处理成字符格式
+使用方法 :
+        安装 sudo pip3 install pillow
+        python3 index.py xxx.png<图片名>
--- updateREADME/index.py
+++ updateREADME/index.py
@@ -1,7 +1,9 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
-
+执行之后会自动更新该项目的README
+名字: 根据文件夹名
+描述: 根据index文件头部的描述信息
 '''
 import os
 # 之类本来想跳转到上级目录, 后来想想直接在上级目录运行这个文件就行了
@@ -16,18 +18,54 @@ def main():
     datalist = []
     for i in os.listdir(root_path):
         if(os.path.isdir(i) and i.find('.') == -1):
-            datalist.append(i)
+            dataObj = {
+                'describe': '暂无描述',
+                'name': i
+            }
 
-    with open('./README.md', 'a+') as f:
+            # 读取注释
+            filePath = os.path.join(root_path, i, 'index.py')
+            with open(filePath) as f:
+                startIndex = None
+                endIndex = None
+                lines = f.readlines()
+                for index, line in enumerate(lines):
+                    if endIndex == None:
+                        if line.startswith("'''"):
+                            if startIndex == None:
+                                startIndex = index
+                            else:
+                                endIndex = index
+                print(startIndex, endIndex)
+
+                # 读取描述信息
+                thedescribe = ''
+                for i in range(startIndex+1, endIndex):
+                    text = lines[i]
+                    if(not text.startswith('@')):
+                        thedescribe += text
+                dataObj['describe'] = thedescribe
+
+            datalist.append(dataObj)
+
+    with open('./README.md', 'w+') as f:
         data = f.read()
+
+        startText = '''# python-practice
+
+记录一下使用python写的一些东西
+
+
+
+---'''
+        f.write(startText)
         for i in datalist:
-            mdText = '''
-            \n* [{}]({}/{}) \n
-                描述
-            '''.format(i, githubRootUrl, i)
-            if data.find(i) == -1:
+            mdText = '''\n* [{}]({}/{}) \n    {}'''.format(i['name'],
+                                                           githubRootUrl, i['name'], i['describe'])
+            if data.find(i['name']) == -1:
                 f.write(mdText)
                 print('添加{} '.format(i))
+        f.close()
 
 
 if __name__ == "__main__":
